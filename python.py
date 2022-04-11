c12adc6bbd76bc30ab1d5c12406300d191026e9f
clw0n <102551574+clw0n@users.noreply.github.com>
2022-04-11 02:31:39 +0200

    Update PyPhone
    Trying first model.

--- python.py
+++ python.py
@@ -1,16 +1,16 @@
-import os, sys, threading
+import os, sys, threading, platform
 from time import sleep
 
 main_msg = """\n\033[0;35m
  _________$$$$$$$$$$
-_________$_________$$
-_________$_$$$$$$$_$$
-_________$_$     $_$$
-_________$_Python$_$$			
-_________$_Termux|_$$			‖	Python Termux Script : execute scripts remotely or execute script ‖
-_________$_$     $_$$           ‖   directly on your phone ! -Developped by Nathan Raymond ‖
-_________$_$$$$$$$_$$ 
-_________$_________$$
+_________$_________ $$
+_________$_$$$$$$$_ $$
+_________$_$     $_ $$
+_________$_Python$_ $$			
+_________$_Termux|_ $$			‖	Python Termux Script : execute scripts remotely or execute script ‖
+_________$_$     $_ $$           ‖   directly on your phone ! -Developped by Nathan Raymond ‖
+_________$_$$$$$$$_ $$ 
+_________$_________ $$
 __________$$$$$$$$$$
 _________$_________$$
 ________$_1__2__3_$$$
@@ -23,6 +23,7 @@ ______$$$$$$$$$
 \033[0;35m\n"""
 
 
+
 bye_msg = "\033[1;Shutting down... \033[1;31m"
 
 if not os.geteuid()==0:
@@ -30,26 +31,49 @@ if not os.geteuid()==0:
 else:
 	sleep(1)
 	print(main_msg)
+	print("="*len(main_msg))
 
 def home():
 	try:
-		def config0(): #configure parameters for options
+		def config(): #configure parameters for options
 			try:
+				global prvaddr
+				global user
+				global macaddr
+				global hostname
+				global prvaddrsrc
 				prvaddr = os.popen("ifconfig | grep broadcast | awk '{print $2}'").read()
-				user = os.getlogin()
-				n_mac = os.popen("ip addr | grep 'state UP' -A1 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'").read()
-				hostname = os.popen("hostname -I").read()
-				netmask = os.popen("ip route show | grep 'default via' | awk '{print $3}'").read()
 				prvaddr = prvaddr[:-1]
-			except: ##implémenter l'erreur (aucune connection internet)
-				print("\033[1;31mYou need to be connected to internet to configure your network !\033[1;31m")
-
+				user = os.getlogin()
+				macaddr = os.popen("ip addr | grep 'state UP' -A1 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'").read()
+				hostname = os.popen("hostname -I").read()
+				prvaddrsrc = os.popen("ip route show | tail -n1 | awk '{print $1}' | cut -f1 -d'/'").read()
+			except: ##implémenter l'erreur (aucune connection internet)~~~
+				print("\033[1;31mYou need to be connected to internet in order to configure your network !\033[1;31m")
+		def bind_ssh(ipaddr):
+			try:
+				os.system("ssh root@{ipaddr}")
+				print("Connected to {}")
+			except ##finish later
+			
+			if platform.system() == "Linux" or "Windows" or "Darwin":
+				print("Computer detected ! Connect phone with ssh ?")
+				choice = raw_input(">>> ")
+				if choice =="y" or "Y":
+					ipaddr = input("Please input the phone's ip to connect with ssh. ")
+					bind_ssh(ipaddr)
+				elif choice =="n" or "N":
+					options()
+				else:
+					print("033[1;31mPlease input a correct value !033[1;31m")
+				
 		def options():
 			print("""1) Scan nearby networks
 					 2) Port scanner
 					 3) Sniff data
 					 4) WPA2 handshake cracking 
-					 5) Exit""")
+					 5) Start background-audio recording (phone ==> computer)
+					 6) Exit""")
 			choice = raw_input(">>> ")
 			def scan0():
 				pass
@@ -59,6 +83,8 @@ def home():
 				pass
 			def wpa2_hs():
 				pass
+			def bg_record():
+				pass
 			if choice == 1:
 				scan0()
 			elif choice == 2:
@@ -67,7 +93,9 @@ def home():
 				sniff()
 			elif choice== 4:
 				wpa2_hs()
-			elif choice ==5:
+		 	elif choice ==5:
+				bg_record()
+			elif choice ==6:
 				sleep(1)
 				sys.exit(bye_msg)
 	except KeyboardInterrupt:
